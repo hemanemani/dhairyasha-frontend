f7edae0ce795af7a319564fa0fe0ac2441458526
hemanemani <hemanemani@orgenik.com>
2025-08-05 18:02:24 +0530

    updated css1

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api",
-  // baseURL: "https://api.shahdhairya.in/api",
+  // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api",
+  baseURL: "https://api.shahdhairya.in/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
